d72c7bbcba63645823ff28620838cde29e52a892
WIllianMartins2018 <willian.martins01@hotmail.com>
2022-06-28 20:02:55 -0300

    Flex Box

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css"
 import Primeiro from './components/basicos/Primeiro'
 import ComParamentro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
@@ -6,19 +7,26 @@ import Card from './components/layout/Card'
 
 export default function App(props) {
     return (
-        <div id="App">
+        <div className="App">
             <h1>Fundamentos React</h1>
 
-            <Card titulo="Desafio Aleatório">
-                <Aleatorio min={1} max={10} />
-            </Card>
-            
-            <Fragmento />
-            <hr></hr>
-            <ComParamentro
-                titulo="Segundo Componente"
-                subtitulo="Muito Legal!!"
-            />
+            <div className='Cards'>
+
+                <Card titulo="Desafio Aleatório">
+                    <Aleatorio min={1} max={10} />
+                </Card>
+
+                <Card titulo="Fragmento">
+                    <Fragmento />
+                </Card>
+
+                <Card titulo="Segundo Componente">
+                    <ComParamentro titulo="Segundo Componente" subtitulo="Muito Legal!!" />
+                </Card>
+
+            </div>
+
+
             <hr></hr>
             <Primeiro></Primeiro>
             <hr></hr>
--- src/components/basicos/Fragmento.jsx
+++ src/components/basicos/Fragmento.jsx
@@ -1,7 +1,6 @@
 export default function Fragmento() {
     return (
         <>
-            <h2>Fragmento</h2>
             <p>Cuidado com esse erro !!!</p>
         </>
     )
